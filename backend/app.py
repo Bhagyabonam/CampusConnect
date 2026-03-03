824c17dcf8b57ccbce127b6b9236cbdef173b2c4
Bhagyabonam <bhagyasribonam@gmail.com>
2026-03-03 19:18:06 +0530

    removed python dotenv

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ from .routes.dashboards.student_dashboard import student_dashboard_bp
 from .routes.dashboards.faculty_dashboard import faculty_dashboard_bp
 from .routes.dashboards.student_coordinator_dashboard import student_coordinator_dashboard_bp
 from .routes.dashboards.event_workflow import event_workflow_bp
-from .config import init_db, seed_data
+from .config import seed_data, init_db
 
 app = Flask(__name__)
 CORS(app)
